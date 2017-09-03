846f02fc0b23e4f7fb0b10517fd85082707e2782
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-09-03 15:34:25 +0100

    Fixed.

--- src/Muqsit/EndPortalFrame.php
+++ src/Muqsit/EndPortalFrame.php
@@ -4,7 +4,7 @@ namespace Muqsit;
 
 use pocketmine\block\EndPortalFrame as PmEndPortalFrame;
 
-class Bedrock extends PmEndPortalFrame{
+class EndPortalFrame extends PmEndPortalFrame{
 	
 	public function getResistance(){
 		return 10;
